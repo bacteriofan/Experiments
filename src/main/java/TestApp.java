c1e732d06f50e26d98235dd0f7eb29d8dc18e5f2
Kseniia <bacteriofan@gmail.com>
2022-03-03 20:53:37 +0300

    Initial commit

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Set;
+
+class TestApp {
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    static String f = "fnldr";
+
+    public static void main(String[] args) {
+        f.hashCode();
+
+        for (int index = 0; true; index++) {
+            System.out.println("Welcome");
+        }
+
+    }
+}
